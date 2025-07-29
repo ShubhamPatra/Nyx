217ba6d53cd4e0388011b836954c95d5f77bc6a3
Shubham Patra <shubhampatra635@gmail.com>
2025-07-29 16:44:28 +0530

    Update useChat.js

--- frontend/src/hooks/useChat.js
+++ frontend/src/hooks/useChat.js
@@ -76,7 +76,7 @@ const useChat = () => {
     try {
       setIsLoading(true);
       
-      const response = await fetch('http://localhost:5000/api/nyx', {
+      const response = await fetch('https://nyx-backend-mn48.onrender.com/api/nyx', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
